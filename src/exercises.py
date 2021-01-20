287dbb94d9b6c424907f0ad7d3accd4ffcaea46d
Nik <sharmanikhil1100@gmail.com>
2021-01-20 12:29:50 +0530

    All unit tests passed

--- src/exercises.py
+++ src/exercises.py
@@ -2,10 +2,28 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    return None
+    input_list_len = len(input_list)
+
+    for i in range(input_list_len//2):
+        tmp = input_list[i]
+        input_list[i] = input_list[input_list_len-i-1]
+        input_list[input_list_len-i-1] = tmp
+
+    return input_list
 
 def count_digits(number):
     """
     Return count of digits
     """
-    return None
+    if number>=0 and number<1:
+        return 0
+    
+    no_digits = 0
+    if number < 0:
+        number = -number
+
+    while number >= 1:
+        no_digits += 1
+        number /= 10
+
+    return no_digits
--- src/string_exercise.py
+++ src/string_exercise.py
@@ -7,25 +7,48 @@ class StringExercise:
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        # index postions : [start,stop,step]
+        return input_str[::-1]
 
     def is_english_vowel(self, character):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
-        return None
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        if character in vowels:
+            return True
+        else:
+            return False
 
     def find_longest_word(self, sentence):
         """
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        if len(sentence)==0:
+            return ''
+        if sentence == ' ':
+            return ''
+
+        sentence_list = sentence.split(' ')
+
+        index = 0
+        sentence_list_wordslen = list(map(lambda x: len(x), sentence_list))
+        max_len = 0
+
+        for i, word_len in enumerate(sentence_list_wordslen):
+            if word_len > max_len:
+                max_len = word_len
+                index = i
+
+        return sentence_list[index]
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        return None
+        text_list = text.split(' ')
+        text_list_wordslen = list(map(lambda x: len(x), text_list))
+        return text_list_wordslen
